0e814c232d4fb1835dd110d6d2fadbd494677130
THUMMALA CHAKRAVARTHI <thummalachakravarthi2002@gmail.com>
2025-09-11 12:39:18 +0530

    EagerInitialization in spring

--- EagerInitialization.java
+++ EagerInitialization.java
@@ -1,2 +1,5 @@
 <bean id="alien" class="org.example.Alien"/>
 <bean id="laptop" class="org.example.Laptop"/>
+ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+//Both Alien and Laptop objects are created immediately, even before you call getBean()
+
