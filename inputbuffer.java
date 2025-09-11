031e9ab48604be129df47ca07114890241b88601
THUMMALA CHAKRAVARTHI <thummalachakravarthi2002@gmail.com>
2025-09-11 09:29:25 +0530

    Update inputbuffer.java

--- inputbuffer.java
+++ inputbuffer.java
@@ -4,3 +4,6 @@ int x = sc.nextInt();
 sc.nextLine();   // consume the leftover newline
 
 String str = sc.nextLine();  // now read the actual line
+// Space (' '): just a blank gap between words on the same line.
+
+// Newline (\n): means move the cursor to the next line (like when you press Enter)
